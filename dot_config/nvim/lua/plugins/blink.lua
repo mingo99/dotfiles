5252d6065931e8e737192ca9bbcd5b0f5f913d93
mingo99 <2839131656@qq.com>
2025-07-05 16:54:06 +0800

    nvim(blink): config blink completion

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -3,11 +3,10 @@ return {
   event = { "BufReadPost", "BufNewFile" },
   dependencies = { "xzbdmw/colorful-menu.nvim", opts = {} },
   opts = {
+    cmdline = { completion = { menu = { auto_show = true } } },
     completion = {
-      documentation = {
-        auto_show = true,
-        window = { border = "rounded" },
-      },
+      list = { selection = { preselect = false, auto_insert = true } },
+      documentation = { auto_show = true, window = { border = "rounded" } },
       menu = {
         draw = {
           columns = { { "kind_icon" }, { "label", gap = 1 } },
@@ -30,21 +29,16 @@ return {
       ["<C-u>"] = { "scroll_documentation_up", "fallback" },
       ["<C-d>"] = { "scroll_documentation_down", "fallback" },
     },
-    signature = {
-      enabled = true,
-      window = { border = "rounded" },
-    },
-    cmdline = {
-      completion = {
-        menu = {
-          auto_show = true,
-        },
+    fuzzy = {
+      implementation = "prefer_rust_with_warning",
+      use_frecency = true,
+      use_proximity = true,
+      sorts = {
+        "exact",
+        "score",
+        "sort_text",
       },
+      prebuilt_binaries = { download = true },
     },
-    -- sources = {
-    --   providers = {
-    --     snippets = { score_offset = 1000 },
-    --   },
-    -- },
   },
 }
