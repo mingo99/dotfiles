dfced3e9603cdd86302655b8f77c718b217815a6
mingo99 <2839131656@qq.com>
2026-02-14 21:58:00 +0800

    wezterm(plugin): delete tabline

--- dot_config/wezterm/plugins.lua
+++ dot_config/wezterm/plugins.lua
@@ -1,10 +1,10 @@
 local plugin = require("wezterm").plugin
 local M = {}
 
-local tabline = plugin.require("https://github.com/michaelbrusegard/tabline.wez")
-tabline.setup()
+-- local tabline = plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+-- tabline.setup()
 function M.apply_to_config(config)
-	tabline.apply_to_config(config)
+	-- tabline.apply_to_config(config)
 end
 
 return M
